faa2b7f2b3b52b3de5d72e55adfda759437a8299
Wei Fu <36355462+garrett4wade@users.noreply.github.com>
2025-12-10 11:06:52 +0800

    fix importing with sapo_loss_fn (#703)

--- areal/utils/functional/__init__.py
+++ areal/utils/functional/__init__.py
@@ -4,6 +4,7 @@ from areal.utils.functional.functional import (
     ppo_actor_loss_fn,
     ppo_critic_loss_fn,
     reward_overlong_penalty,
+    sapo_loss_fn,
 )
 from areal.utils.functional.vocab_parallel import (
     gather_logprobs,
@@ -17,6 +18,7 @@ __all__ = [
     "ppo_actor_loss_fn",
     "ppo_critic_loss_fn",
     "reward_overlong_penalty",
+    "sapo_loss_fn",
     # logprobs.py
     "gather_logprobs",
     "gather_logprobs_entropy",
