5d9842fbf5ee4c6c743e10678b4ddb548b039a79
nuzant <meizhiyu.mzy@antgroup.com>
2026-01-07 18:49:41 +0800

    [Bug Fix] Fix import error introduced by tree training PR (#808)
    Fix import error introduced by tree training PR

--- areal/models/tree_attn/module.py
+++ areal/models/tree_attn/module.py
@@ -162,7 +162,7 @@ class PytorchFlexAttention(torch.nn.Module):
 
 
 @contextmanager
-def patch_bridge_for_tree_attn(enable: bool = True):
+def patch_bridge_for_tree_training(enable: bool = True):
     """Context manager to patch LLMBridge for tree training with arbitrary attention mask.
 
     Parameters
@@ -176,7 +176,7 @@ def patch_bridge_for_tree_attn(enable: bool = True):
 
     Examples
     --------
-    >>> with patch_bridge_for_tree_attn(enable=True):
+    >>> with patch_bridge_for_tree_training(enable=True):
     ...     # LLMBridge is patched here
     ...     model = create_model()
     ... # Patch is reverted after exiting the context
